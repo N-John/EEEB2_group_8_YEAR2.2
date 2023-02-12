e8a4fc253706013f051c72e2ab8359f0e0d0bacf
JOHN N <nganga.n.john@gmail.com>
2023-02-12 11:14:19 +0300

    QUESTION 4 UPDATE

--- Assignment2/cmp_question4/main.c
+++ Assignment2/cmp_question4/main.c
@@ -1,12 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
+/*
+  GROUP 8 ASSIGNMENT 2
 
-int main(void){
-    char n[] = "0123456789";
-    int c = 0;
-    for (c = 0;c < 10; c++){
-        putchar(n[c]);
-        printf("\n", c);
+ 4) write a program that prints all single digit numbers of base 10 starting from 0,
+    followed by new line
+    .ALL YOUR CODE SHOULD BE IN THE (main) FUNCTION
+*/
+
+int main(void){                    //begin main
+    int c = 0;                     //initialize the number
+
+    while(1){                      //introduce a loop to cycle through all numbers
+            printf("%d", c);       //print decimal digits beginning from c = 0
+            if(c >= 9){            //since there is no single digit above 9
+                printf("\n");      //introduce new line
+                break;             //stop the infinite loop
+            }else{
+            c++;                   // add a digit if the number is still single digit
+            }
     }
-    return 0;
+    return 0;                      // end
 }
