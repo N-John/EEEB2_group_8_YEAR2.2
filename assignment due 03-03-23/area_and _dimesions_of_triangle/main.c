5c877770e32760fa437752e5c1ffd90711fe6b8c
N-John <nganga.n.john@gmail.com>
2023-03-02 23:21:30 +0300

    commenting

--- assignment due 03-03-23/area_and _dimesions_of_triangle/main.c
+++ assignment due 03-03-23/area_and _dimesions_of_triangle/main.c
@@ -10,26 +10,23 @@ int main()
     int length = 0;
     int hight = 0;
     int area = 0;
-
+    //get length
     printf("WHAT IS THE LENGTH? ");
     scanf("%d", &length);
-
+    //check if length is acceptable
     if(length<1){
             printf("LENGTH CANNOT BE %d \n", length);
-
         }
-
+    // get height
     printf("WHAT IS THE HIGHT? ");
     scanf("%d",&hight);
-
+    //check if height is feasable
     if(hight<1){
         printf("WIDTH CANNOT BE %d \n", hight);
     }
-
-
-
+    //calculate area
     area = 0.5 * length * hight;
-
+    // print error
     printf("\nAREA IS %d WITH LENGTH %d AND HIGHT %d \n", area, length, hight);
 
     return 0;
