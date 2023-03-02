746a323e0982990eadb99fb88ad878e1a4d08c91
N-John <nganga.n.john@gmail.com>
2023-03-02 23:26:48 +0300

    commenting

--- assignment due 03-03-23/lowercase_to_uppercase/main.c
+++ assignment due 03-03-23/lowercase_to_uppercase/main.c
@@ -8,17 +8,17 @@
 
 int main()
 {
-	int x = 0;
+	int x = 0;                         //to count length of word
 	char lower[] = "input";
 	char change;
-
+    //get input
 	printf("INPUT YOUR WORD __");
     scanf("%s", &lower);
 
-	while (lower[x]) {
-		change = lower[x];
-		putchar(toupper(change));
-		x++;
+	while (lower[x]) {                 //open while loop
+		change = lower[x];             //relate each character to change
+		putchar(toupper(change));      //use toupper()toconvert lowercase to uppercase
+		x++;                           //increase count by 1
 	}
 
 	return 0;
