fb7bef026d7b40eeef098620e7913c8f38173324
N-John <nganga.n.john@gmail.com>
2023-03-02 23:31:11 +0300

    commenting

--- examples and assignment/area_and _dimesions_of_triangle/main.c
+++ examples and assignment/area_and _dimesions_of_triangle/main.c
@@ -1,31 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+/*
+ write a c program that will accept the dimensions of a rectangle
+ and calculate its area through the use of a function. The program
+ should then output the dimensions and area in the main prgram.
+*/
 int main()
 {
     int length = 0;
     int hight = 0;
     int area = 0;
-
+    //get length
     printf("WHAT IS THE LENGTH? ");
     scanf("%d", &length);
-
+    //check if length is acceptable
     if(length<1){
             printf("LENGTH CANNOT BE %d \n", length);
-
         }
-
+    // get height
     printf("WHAT IS THE HIGHT? ");
     scanf("%d",&hight);
-
+    //check if height is feasable
     if(hight<1){
         printf("WIDTH CANNOT BE %d \n", hight);
     }
-
-
-
+    //calculate area
     area = 0.5 * length * hight;
-
+    // print error
     printf("\nAREA IS %d WITH LENGTH %d AND HIGHT %d \n", area, length, hight);
 
     return 0;
--- examples and assignment/quadratic equations/main.c
+++ examples and assignment/quadratic equations/main.c
@@ -1,37 +1,39 @@
+/*
+  write a program to read a,b and c in a quadratic equation, determine
+  the root and display a,b,c and the roots
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 
-int getno(char *x){
+int getno(char *x){ //get the number for each value
     int v = 0;
     printf("WHAT IS %s ", x);
     scanf("%d", &v);
     return v;
 }
 
-
 int main()
-{ int a = 0;
-  int b = 0;
-  int c = 0;
+{ int a = 0;  //a
+  int b = 0;  //b
+  int c = 0;  //c
+  int r1 = 0; //root 1
+  int r2 = 0; //root2
+  int x = 0;
+  int d = 0;  //determinant
 
   printf("(a)X2 + (b)X + (c) = 0 \n");
-  a = getno("a");
+  a = getno("a");                              //get a
   printf("%dx2 + (b)X + (c) = 0 \n",a);
-  b = getno("b");
+  b = getno("b");                              //get b
   printf("%dx2 + %dx + (c)= 0 \n",a, b);
-  c = getno("c");
+  c = getno("c");                              // get c
   printf("%dx2 + %dx +%d = 0 \n",a, b, c);
 
-    int r1 = 0;
-    int r2 = 0;
-    int x = 0;
-    int d = 0;
-
-    d = (b*b) - (4*a*c);
-    x =(int)(pow(d,0.5) + 1e-9);
-    r1 = (-b + x) / (a*2);
-    r2 = (-b - x) / (a*2);
-
+    d = (b*b) - (4*a*c);                       //get determinant
+    x =(int)(pow(d,0.5) + 1e-9);               //solve the numerator
+    r1 = (-b + x) / (a*2);                     //get root 1
+    r2 = (-b - x) / (a*2);                     //get root 2
 
   printf("WITH a AS %d, b AS %d AND c AS %d\n", a, b, c);
   if(d >0){
@@ -43,8 +45,5 @@ int main()
   if(d ==0){
     printf(" EQUATION HAS ONLY 1 DISTINCT ROOT%d\n", r1);
   }
-
-
-
   return 0;
 }
