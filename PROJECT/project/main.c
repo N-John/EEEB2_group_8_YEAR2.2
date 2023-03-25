25f8a2609a699d9e4135bdb6be99e0f0d43594a7
32io <bravishe@gmail.com>
2023-03-26 02:42:01 +0300

    reference point

--- PROJECT/project/main.c
+++ PROJECT/project/main.c
@@ -1,5 +1,60 @@
 #include <stdio.h>
 #include <stdlib.h>
+/*
+Develop an expense tracker
+
+    Write expenses
+    Log expense
+    Statistic
+        Average spenditure/input
+        Max/min spenditure/input
+Advice on expenditure
+    Thresholds setting
+Include features such as M-Pesa integration and the ability to record cash transactions manually
+
+    Record entering manually (linked to no 1)
+    Mpesa intergrations unavaible
+Ensure that the program is user-friendly and easy to use
+
+    linked to number 1 and 2
+Ensure that the program is secure, reliable, and scalable and that it can support a large number of users
+
+    encryption
+Pleasure the impact of the program on SMEs in terms of improved financial management, increased profitability, and greater access to financing.
+    testing
+
+*/
+
+
+
+//THIS FILE OFFERS REFENCE TO VARIOUS FUNCTIONS NECESSARY
+
+//CHANGES ARE ALLOWED
+void encrypt{
+
+
+}
+void decrypt{
+
+
+}
+void  give_Advice(){
+
+
+}
+
+void write_expenses{
+
+
+}
+void get_expenses{
+
+
+}
+void statistics{
+
+
+}
 
 int main()
 {
